566569cccebd659cedef4d515fe5f75e09ee96dc
link <cxwl.h@qq.com>
2020-10-12 20:52:47 +0800

    初始化

---
+++ src/main/java/top/link666/thedate/ThedateApplication.java
@@ -0,0 +1,13 @@
+package top.link666.thedate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThedateApplication.class, args);
+    }
+
+}
